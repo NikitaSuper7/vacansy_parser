6b5afdca6cb7086055b065aac637801b155a4034
Nike_S7 <nike7cr77@ya.ru>
2024-04-10 10:24:26 +0300

    add user_func

---
+++ main.py
@@ -0,0 +1,28 @@
+from src.vacansy import Vacansy
+from src.hh import HH
+from src.saver import Saver
+import json
+
+
+def user_interaction():
+    """Осуществляет взаимодействие с пользователем."""
+    search_request = input("Please enter your search request: ").split()
+    how_many = int(input("Please enter  TOP N vacancies where 'N' is number vacancies amount: "))
+    request_1 = HH()
+    request_1.get_response(search_request)
+
+    Vacansy.make_objects(request_1)
+    sort_vacancies = Vacansy.sorter_salary()
+    save_vac = Saver(sort_vacancies)
+    save_vac.json_saver()
+
+    with open('src/vacansies.json', 'r', encoding='utf-8') as file:
+        respond = json.load(file)
+        respond = respond[0:how_many]
+
+    print(respond, len(respond), sep='\n')
+    return respond
+
+
+if __name__ == '__main__':
+    user_interaction()
