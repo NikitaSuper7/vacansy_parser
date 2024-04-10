8bb626eafba1c805f2eb4512ceaaf8c7cf11ed4c
Nike_S7 <nike7cr77@ya.ru>
2024-04-10 14:47:14 +0300

    change split() to strip() in main.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from src.vacansy import Vacansy
 from src.hh import HH
 from src.saver import Saver
-import json
+from src.abstract_classes import Parser
+import requests
 
 
 def user_interaction():
     """Осуществляет взаимодействие с пользователем."""
-    search_request = input("Please enter your search request: ").split()
+    search_request = input("Please enter your search request: ").strip()
     how_many = int(input("Please enter  TOP N vacancies where 'N' is number vacancies amount: "))
     request_1 = HH()
     request_1.get_response(search_request)
@@ -15,17 +16,7 @@ def user_interaction():
     Vacansy.sorter_salary()
     str_vacansy = Saver(Vacansy.all_vacancies)
     str_vacansy.json_saver()
-
-    with open('src/vacansies.json', 'r', encoding='utf-8') as file:
-        responds = json.load(file)
-        responds = responds[0:how_many]
-    rang_vac = 1
-    for respond in responds:
-        print(f"\nRang № {rang_vac}")
-        rang_vac += 1
-        for key, value in respond.items():
-            print(key, value, end='\n')
-    return responds
+    str_vacansy.json_loader(how_many=how_many)
 
 
 if __name__ == '__main__':
