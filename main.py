da8d2df75520f25e46780a1b19e520967f134570
Nike_S7 <nike7cr77@ya.ru>
2024-04-11 09:17:27 +0300

    refactor main function

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 from src.vacansy import Vacansy
 from src.hh import HH
-from src.saver import Saver
-from src.abstract_classes import Parser
-import requests
+from src.saver import JsonSaver
+
 
 
 def user_interaction():
     """Осуществляет взаимодействие с пользователем."""
     search_request = input("Please enter your search request: ").strip()
     how_many = int(input("Please enter  TOP N vacancies where 'N' is number vacancies amount: "))
+    salary_range = input("Please enter a salary range in format 'from-to': ").strip().split(sep='-')
     request_1 = HH()
     request_1.get_response(search_request)
 
-    Vacansy.make_objects(request_1)
-    Vacansy.sorter_salary()
-    str_vacansy = Saver(Vacansy.all_vacancies)
+    Vacansy._make_objects(request_1)
+    Vacansy._sorter_salary()
+    Vacansy._salary_range(salary_range)
+    str_vacansy = JsonSaver(Vacansy.all_vacancies)
     str_vacansy.json_saver()
     str_vacansy.json_loader(how_many=how_many)
 
